d5be89ddc00a7123c2e770cc9a962215f7a70ce4
scffs <67464545+scffs@users.noreply.github.com>
2022-10-08 18:09:14 +0700

    added some adaptive

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,5 +4,7 @@ let themeButton = document.querySelector('.btn-theme');
 
 themeButton.onclick = function() {
     page.classList.toggle('dark-theme');
+    page.classList.toggle('light-theme');
     card.classList.toggle('dark-theme');
+    card.classList.toggle('light-theme');
 };
