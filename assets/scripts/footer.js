1838c1667533407dc5d9cabfa9abe8404d91ea09
scffs <67464545+scffs@users.noreply.github.com>
2022-10-08 18:49:02 +0700

    some fixes

--- assets/scripts/footer.js
+++ assets/scripts/footer.js
@@ -1,6 +1,6 @@
 let footerText = `
          <div class="container">
-            <a href="https://github.com/scffs" target="_blank"><img src="assets/img/GH.png" height="48" alt="GitHub"></a>
+            <a href="https://github.com/scffs" target="_blank"><img id="Git" src="assets/img/GH.png" height="48" alt="GitHub"></a>
             <a href="https://t.me/sc0ffs" target="_blank"><img src="assets/img/TGW.png" height="48" alt="TG"></a>
             <a href="https://vk.com/scoffs" target="_blank"><img src="assets/img/VKW.svg" height="48" alt="VK"></a>
         </div>
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,16 @@
 let page = document.querySelector('.page');
 let card = document.querySelector('.info-menu');
 let themeButton = document.querySelector('.btn-theme');
+let body = document.querySelector('.light-theme');
 
 themeButton.onclick = function() {
     page.classList.toggle('dark-theme');
     page.classList.toggle('light-theme');
     card.classList.toggle('dark-theme');
     card.classList.toggle('light-theme');
+    if (body.classList.contains('light-theme')){
+        document.getElementById("Git").src="assets/img/GH.png";
+    }else{
+        document.getElementById("Git").src="assets/img/GitHub-Mark-Light-120px-plus.png";
+    }
 };
